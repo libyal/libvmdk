e390a39f8380d94215873f032236af7b38f6f2bc
Joachim Metz <joachim.metz@gmail.com>
2015-01-05 18:24:54 +0100

    Applied updates.

--- vmdktools/vmdkoutput.c
+++ vmdktools/vmdkoutput.c
@@ -1,7 +1,7 @@
 /*
  * Common output functions for the vmdktools
  *
- * Copyright (c) 2009-2014, Joachim Metz <joachim.metz@gmail.com>
+ * Copyright (C) 2009-2015, Joachim Metz <joachim.metz@gmail.com>
  *
  * Refer to AUTHORS for acknowledgements.
  *
@@ -47,7 +47,7 @@ void vmdkoutput_copyright_fprint(
 	 */
 	fprintf(
 	 stream,
-	 _( "Copyright (c) 2009-2014, %s.\n" ),
+	 _( "Copyright (C) 2009-2015, %s.\n" ),
 	 _( "Joachim Metz" ) );
 
 	fprintf(
