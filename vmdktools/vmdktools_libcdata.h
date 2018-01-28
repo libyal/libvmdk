b9013319ec8376bd81b7bd6348296de9edc6418c
Joachim Metz <joachim.metz@gmail.com>
2018-01-28 11:26:31 +0100

    Applied updates

--- vmdktools/vmdktools_libcdata.h
+++ vmdktools/vmdktools_libcdata.h
@@ -1,7 +1,7 @@
 /*
  * The internal libcdata header
  *
- * Copyright (C) 2009-2017, Joachim Metz <joachim.metz@gmail.com>
+ * Copyright (C) 2009-2018, Joachim Metz <joachim.metz@gmail.com>
  *
  * Refer to AUTHORS for acknowledgements.
  *
@@ -48,7 +48,7 @@
 
 #include <libcdata.h>
 
-#endif
+#endif /* defined( HAVE_LOCAL_LIBCDATA ) */
 
-#endif
+#endif /* !defined( _VMDKTOOLS_LIBCDATA_H ) */
 
