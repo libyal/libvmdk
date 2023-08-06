821f60b4bcbb84e3bbb9ec76147c6b4b43070041
Joachim Metz <joachim.metz@gmail.com>
2023-08-06 08:47:02 +0200

    Applied updates

--- libvmdk/libvmdk_handle.c
+++ libvmdk/libvmdk_handle.c
@@ -935,9 +935,9 @@ int libvmdk_handle_open_file_io_handle(
 {
 	libvmdk_internal_handle_t *internal_handle = NULL;
 	static char *function                      = "libvmdk_handle_open_file_io_handle";
+	uint8_t file_io_handle_opened_in_library   = 0;
 	int bfio_access_flags                      = 0;
 	int file_io_handle_is_open                 = 0;
-	int file_io_handle_opened_in_library       = 0;
 	int result                                 = 1;
 
 	if( handle == NULL )
